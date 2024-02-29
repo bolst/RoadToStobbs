d96449b44f7d292724143cc2d7ba4305137a3ee6
bolst <bolton21@uwindsor.ca>
2024-02-29 17:04:51 -0500

    heheh

--- RoadToStobbs/obj/Debug/net6.0/RoadToStobbs.AssemblyInfo.cs
+++ RoadToStobbs/obj/Debug/net6.0/RoadToStobbs.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     " a component library built on top of Blazor and CSS frameworks like Bootstrap, T" +
     "ailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+7a91852f37a8b65fd072b39bd47bd9005a33b790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+842b1f67ce632050efeeafbad4a41fab9c35b74d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadToStobbs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadToStobbs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
